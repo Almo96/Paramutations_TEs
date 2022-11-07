334a3f38674aabc29d43281d1d9ce1815340069c
Almorò Scarpa <ascarpa@i122srv47.vu-wien.ac.at>
2022-11-07 11:04:10 +0100

    New files

---
+++ Simulation/2022_11_07_Simulation_7_Storm.md
@@ -0,0 +1,127 @@
+2022_11_07_Simulation_7\_Storm
+================
+Almo
+2022-11-07
+
+## Introduction
+
+With this simulation we wanted to understand the role of paramutations
+on minimu fitness during a TEs invasion.
+
+### Initial conditions:
+
+A population of 1000, 5 chromosomes of size 10 Mb
+
+Every dot in the red to green gradient is a simulation at generation
+5000, or less if it failed. If it failed due to the extinction of the
+population the dot will be white, if it failed because all TEs were
+removed from the population it will be grey.
+
+## Materials & Methods
+
+version: invadego0.2.3
+
+### Commands for the simulation:
+
+The simulations were generated using the code from:
+
+-   simstorm_para_vs_selection
+-   simstorm_para_vs_clusters
+
+### Visualization in R
+
+Setting the environment
+
+``` r
+library(tidyverse)
+library(ggplot2)
+theme_set(theme_bw())
+```
+
+Visualization:
+
+``` r
+setwd("/Users/ascarpa/Paramutations_TEs/Simulation/Raw")
+
+df<-read.table("output_u0.2_clu100_final", sep = "\t", fill = TRUE, row.names=NULL)
+df_2<-read.table("output_u0.1_x0.01_clu0.1_250", sep = "\t", fill = TRUE, row.names=NULL)
+
+
+naming <- c("rep", "gen", "popstat", "fmale", "spacer_1", "fwte", "avw", "min_w", "avtes", "avpopfreq", "fixed",
+            "spacer_2", "phase", "fwpirna", "spacer_3", "fwcli", "avcli", "fixcli", "spacer_4", "fwpar_yespi",
+            "fwpar_nopi", "avpar","fixpar","spacer_5","piori","orifreq","spacer 6", "sampleid_x", "sampleid_para","extra")
+
+naming_2 <- c("rep", "gen", "popstat", "fmale", "spacer_1", "fwte", "avw", "min_w", "avtes", "avpopfreq", "fixed",
+              "spacer_2", "phase", "fwpirna", "spacer_3", "fwcli", "avcli", "fixcli", "spacer_4", "fwpar_yespi",
+              "fwpar_nopi", "avpar","fixpar","spacer_5","piori","orifreq","spacer 6", "sampleid_clu", "sampleid_para","extra")
+
+names(df) <- naming
+names(df_2) <- naming_2
+
+
+df$sampleid_x <- as.numeric(df$sampleid_x)
+df$sampleid_para <- as.numeric(df$sampleid_para)
+df$min_w <- as.numeric(df$min_w)
+
+df_2$sampleid_clu <- as.numeric(df_2$sampleid_clu)
+df_2$sampleid_para <- as.numeric(df_2$sampleid_para)
+df_2$min_w <- as.numeric(df_2$min_w)
+
+#Keep only last generation, will be less then 5000 if fail
+df<-subset(df, gen > 0)
+df_2<-subset(df_2, gen > 0)
+
+
+color.gradient <- function(x, colors=c("#D7191C","#FDAE61","#A6D96A","#1A9641"), colsteps=100) {
+  return( colorRampPalette(colors) (colsteps) [ findInterval(x, seq(min(df$min_w),1.0, length.out=colsteps)) ] )
+}
+
+color.gradient_2 <- function(x, colors=c("#D7191C","#FDAE61","#A6D96A","#1A9641"), colsteps=100) {
+  return( colorRampPalette(colors) (colsteps) [ findInterval(x, seq(min(df_2$min_w),1.0, length.out=colsteps)) ] )
+}
+```
+
+``` r
+df$col<-color.gradient(df$min_w)
+df[df$popstat=="fail-0",]$col<-"grey"
+df$col<-as.factor(df$col)
+
+df_2$col<-color.gradient_2(df_2$min_w)
+df_2[df_2$popstat=="fail-w",]$col<-"white"
+df_2[df_2$popstat=="fail-0",]$col<-"grey"
+df_2$col<-as.factor(df_2$col)
+```
+
+``` r
+g_para_selection<-ggplot(df,aes(x=sampleid_para/1000,y=sampleid_x,color=col))+scale_color_manual(values=levels(df$col))+
+  geom_point(alpha=0.7,size=0.7)+scale_y_log10()+
+  scale_x_continuous(labels = scales::percent)+
+  ylab("Negative selection coefficient")+
+  xlab("% paramutable loci per genome")+
+  theme(legend.position = "none",panel.background = element_rect(fill="grey90"))
+
+plot(g_para_selection)
+```
+
+![](2022_11_07_Simulation_7_Storm_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+``` r
+g_para_cluster<-ggplot(df_2,aes(x=sampleid_para/1000,y=sampleid_clu/10000,color=col))+scale_color_manual(values=levels(df_2$col))+
+  geom_point(alpha=0.7,size=0.7)+
+  scale_y_continuous(labels = scales::percent)+
+  scale_x_continuous(labels = scales::percent)+
+  ylab("Cluster size as % of the genome")+
+  xlab("% paramutable loci per genome")+
+  theme(legend.position = "none",panel.background = element_rect(fill="grey90"))
+
+plot(g_para_cluster)
+```
+
+![](2022_11_07_Simulation_7_Storm_files/figure-gfm/unnamed-chunk-4-2.png)<!-- -->
+
+## Conclusions
+
+There is a clear role in protecting the population from a serious
+decrease of fitness. Given all other conditions equal a simulation with
+more paramutable loci will have a higher minimum fitness, therefore the
+chance for extinction is highly reduced.
---
+++ Simulation/Extra_files_py.py/simstorm_para_vs_clusters.py
@@ -0,0 +1,135 @@
+import argparse
+import random
+import time
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+#--max-ins 10000
+def get_basis(invade):
+    return " {0} -no-x-cluins --N 1000 --gen 5000 --genome mb:10,10,10,10,10 --x 0.01 --rr 4,4,4,4,4 --rep 1 --u 0.1 --basepop 100 --steps 5000 --silent".format(invade)
+
+def get_filter():
+    return """|grep -v "^Invade"|grep -v "^#" """
+
+def get_rand_para():
+    r=random.randint(1,999)
+    a=""
+    for x in range(0,r):
+        a+=f"{x},"
+    a=a[:-1]
+    a = a + " "
+    return f"1000:{a}"
+
+def get_rand_clusters():
+    r=random.randint(1,250)
+    return f"{r},{r},{r},{r},{r}"
+
+
+def run_cluster_negsel(invade,count,output):
+    """
+    TE invasion that is stopped by cluster insertions and neg selection against TEs
+    """
+    basis =get_basis(invade) 
+    commandlist=[]
+    for i in range(0,count):
+        x=get_rand_clusters()
+        u=get_rand_para()
+        tr=current_milli_time()+i
+        #command=basis+" --x {0} --paramutation {1} --replicate-offset {2} --file-tally tally{2}.txt --file-sfs sfs{2}.txt --file-mhp mhp{2}.txt --seed {3} ".format(x,u,i,tr)
+        command=basis+" --cluster kb:{0} --paramutation {1} --replicate-offset {2} --seed {3} ".format(x,u,i,tr)
+        #ri=random.random()
+        arr = u.split(',')
+        lastWord_para = arr[- 1]
+        arr_2 = x.split(',')
+        lastWord_cluster = arr_2[- 1]
+        command+= "--sampleid \"{0}\t{1}\"  {2} > {3}{4}".format(lastWord_cluster,lastWord_para,get_filter(),output,i)
+        commandlist.append(command)
+    return commandlist;
+
+
+parser = argparse.ArgumentParser(description="""           
+Description
+-----------
+    Simulation storm""",formatter_class=argparse.RawDescriptionHelpFormatter,
+epilog="""
+Prerequisites
+-------------
+    python version 3+
+
+Authors
+-------
+    Robert Kofler
+    Filip Wierzbicki
+    Almorò Scarpa
+""")
+
+
+parser.add_argument("--number", type=int, required=True, dest="count", default=None, help="the number of simulations")
+parser.add_argument("--threads", type=int, required=True, dest="threads", default=None, help="the threads of simulations")
+parser.add_argument("--output", type=str, required=True, dest="output", default=None, help="the outputfile of simulations")
+parser.add_argument("--invade", type=str, required=True, dest="invade", default=None, help="the invade.go")
+# parser.add_argument("--type", type=str, required=True, dest="type", default=None, help="nostop|ns|nst|cluster|nscluster")
+parser.add_argument("--silent",action="store_true", dest="silent", help="be quit; default=False")
+
+
+
+args = parser.parse_args()
+#0   1    2    3     4     5    6       7    8    9           10     11   1     13   14        15   16   17   18   19   20       21   22        23 
+#1	0	|	0.01	1.00	0.01	0.0005	0	|	0.0000	0.00	0.0000	0	|	0.0000	10	0	0.10	0.00	1000	|	u=0.01	ci=1		1
+#1	100	|	0.11	1.00	0.12	0.0074	0	|	0.0000	0.00	0.0000	0	|	0.0010	8	0	0.35	0.00	1000	|	u=0.01	ci=1		1
+#1	200	|	0.40	1.00	0.50	0.0075	0	|	0.0270	0.03	0.0070	0	|	0.0060	33	2	0.70	0.17	1000	|	u=0.01	ci=1		1
+#1	300	|	0.84	1.00	1.76	0.0092	0	|	0.2700	0.30	0.0370	0	|	0.0090	96	4	1.33	0.51	1000	|	u=0.01	ci=1		1
+
+commandlist=run_cluster_negsel(args.invade,args.count,args.output)
+
+"""
+if(args.type=="nostop"):
+    commandlist=run_nostop(args.invade,args.count,args.output)
+elif(args.type=="ns"):
+    commandlist=run_negsel(args.invade,args.count,args.output)
+elif(args.type=="nst"):
+    commandlist=run_negsel_t(args.invade,args.count,args.output)
+elif(args.type=="cluster"):
+    commandlist=run_cluster(args.invade,args.count,args.output)
+elif(args.type=="nscluster"):
+    commandlist=run_cluster_negsel(args.invade,args.count,args.output)
+else:
+    raise Exception("Unknown simulation type")
+"""
+    
+    
+
+
+
+ 
+def submit_job_max_len(commandlist, max_processes):
+    import subprocess
+    import time
+    sleep_time = 10.0
+    processes = list()
+    for command in commandlist:
+        if(not args.silent):
+            print ('running {n} processes. Submitting {proc} '.format(n=len(processes),proc=str(command)))
+        processes.append(subprocess.Popen(command, shell=True, stdout=None))
+        while len(processes) >= max_processes:
+            time.sleep(sleep_time)
+            processes = [proc for proc in processes if proc.poll() is None]
+    while len(processes) > 0:
+        time.sleep(sleep_time)
+        processes = [proc for proc in processes if proc.poll() is None]
+ 
+submit_job_max_len(commandlist, max_processes=args.threads)
+print ("Done")
+    
+
+
+    
+
+
+
+
+
+
+
+
