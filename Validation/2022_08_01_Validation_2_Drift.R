30d603dce3376899225507fc119458b0782cbdfe
Almorò Scarpa <ascarpa@i122srv47.vu-wien.ac.at>
2023-01-24 13:24:21 +0100

    Update

--- Validation/2022_08_01_Validation_2_Drift.R
+++ Validation/2022_08_01_Validation_2_Drift.R
@@ -12,20 +12,39 @@ gl_1<-ggplot(data_1,aes(x=fixed))+
       geom_histogram(binwidth = 1)+
       ylim(0,65)+
       ggtitle("Population 250")+
-      geom_vline(xintercept=20, lwd=1,lty=2, colour="blue")
+      geom_vline(xintercept=20, lwd=1,lty=2, colour="blue")+
+      theme(plot.title = element_text(size=24),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.x = element_text(size=24),
+        axis.title.y = element_text(size=24),
+        strip.text = element_text(size = 24))
 
 data_2 <- validation[which(validation$sampleid == "pd500"),names(validation) %in% c("rep","fixed")]
 gl_2<-ggplot(data_2,aes(x=fixed))+
       geom_histogram(binwidth = 1)+
       ylim(0,65)+
       ggtitle("Population 500")+
-      geom_vline(xintercept=10, lwd=1,lty=2, colour="blue")
+      geom_vline(xintercept=10, lwd=1,lty=2, colour="blue")+
+      theme(plot.title = element_text(size=24),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.x = element_text(size=24),
+        axis.title.y = element_text(size=24),
+        strip.text = element_text(size = 24))
 
 data_3 <- validation[which(validation$sampleid == "pd1000"),names(validation) %in% c("rep","fixed")]
 gl_3<-ggplot(data_3,aes(x=fixed))+
       geom_histogram(binwidth = 1)+
       ylim(0,65)+
       ggtitle("Population 1000")+
-      geom_vline(xintercept=5, lwd=1,lty=2, colour="blue")
+      geom_vline(xintercept=5, lwd=1,lty=2, colour="blue")+
+      theme(plot.title = element_text(size=24),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.x = element_text(size=24),
+        axis.title.y = element_text(size=24),
+        strip.text = element_text(size = 24))
+
 
 gl_1+gl_2+gl_3
