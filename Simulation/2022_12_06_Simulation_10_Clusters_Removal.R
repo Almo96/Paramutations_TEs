a3ec878fec2622c434d2821351ea04ff38d2b710
Almorò Scarpa <ascarpa@i122srv47.vu-wien.ac.at>
2023-01-25 17:25:48 +0100

    Update

--- Simulation/2022_12_06_Simulation_10_Clusters_Removal.R
+++ Simulation/2022_12_06_Simulation_10_Clusters_Removal.R
@@ -22,24 +22,26 @@ g<-ggplot()+
   geom_vline(xintercept = 2000, linetype="dashed", color = "black", linewidth = 0.7)+
   xlab("generation")+
   ylab("TEs insertions per diploid individual")+
-  theme( axis.text = element_text(size = 36),
-         axis.text.x = element_text(size = 20),
-         axis.title = element_text(size = 36, face = "bold"),
-         legend.position="none",
-         strip.text = element_text(size = 36))+
+  theme(plot.title = element_text(size=24),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.x = element_text(size=24),
+        axis.title.y = element_text(size=24),
+        strip.text = element_text(size = 24),
+        legend.position="none")+
   scale_colour_manual(values=p)+
   scale_x_continuous(breaks = seq(0, 5000, by = 2500))+
   facet_wrap(~sampleid, ncol = 5, nrow = 2, labeller = labeller(sampleid = 
-                                                                  c("p0_0" = "para = 0%, clusters -0 ",
-                                                                    "p0_1" = "para = 0%, clusters -1 ",
-                                                                    "p0_2" = "para = 0%, clusters -2 ",
-                                                                    "p0_3" = "para = 0%, clusters -3 ",
-                                                                    "p0_4" = "para = 0%, clusters -4 ",
-                                                                    "p10_0" = "para = 10%, clusters -0 ",
-                                                                    "p10_1" = "para = 10%, clusters -1 ",
-                                                                    "p10_2" = "para = 10%, clusters -2 ",
-                                                                    "p10_3" = "para = 10%, clusters -3 ",
-                                                                    "p10_4" = "para = 10%, clusters -4 ")))
+                                                                  c("p0_0" = "p = 0%, clu -0 ",
+                                                                    "p0_1" = "p = 0%, clu -1 ",
+                                                                    "p0_2" = "p = 0%, clu -2 ",
+                                                                    "p0_3" = "p = 0%, clu -3 ",
+                                                                    "p0_4" = "p = 0%, clu -4 ",
+                                                                    "p10_0" = "p = 10%, clu -0 ",
+                                                                    "p10_1" = "p = 10%, clu -1 ",
+                                                                    "p10_2" = "p = 10%, clu -2 ",
+                                                                    "p10_3" = "p = 10%, clu -3 ",
+                                                                    "p10_4" = "p = 10%, clu -4 ")))
 
 
 plot(g)
